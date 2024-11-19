269f0770a3f31df5ea6a3bffda7e34c55ff576c5
A <kingiswinter@gmail.com>
2024-11-19 22:03:53 +0800

    feat: 重构项目并添加示例代码
    - 重构了项目的目录结构，将源码从 src 目录移至根目录
    - 添加了 README.md 文件，包含项目介绍和示例代码
    - 更新了 civitai_api.dart 文件，增加了新的 API 方法
    - 修改了测试文件，添加了示例调用代码

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# civitai_api
+[Civitai Api](https://github.com/civitai/civitai/wiki/REST-API-Reference)
+
+``` dart
+final CivitaApi api = CivitaApi('api_key');
+
+api.getModelVersionById(128713).then((resp) {
+    print(resp?.toJson());
+});
+```
--- test/civitai_api_test.dart
+++ test/civitai_api_test.dart
@@ -2,10 +2,17 @@
  * @Author: A kingiswinter@gmail.com
  * @Date: 2024-11-13 23:52:09
  * @LastEditors: A kingiswinter@gmail.com
- * @LastEditTime: 2024-11-14 23:16:23
+ * @LastEditTime: 2024-11-19 22:01:59
  * @FilePath: /civitai_api/test/civitai_api_test.dart
  * 
  * Copyright (c) 2024 by A kingiswinter@gmail.com, All Rights Reserved.
  */
 
-void main() {}
+import 'package:civitai_api/civitai_api.dart';
+
+void main() {
+  final CivitaApi api = CivitaApi('api_key');
+  api.getModelVersionById(128713).then((resp) {
+    print(resp?.toJson());
+  });
+}
